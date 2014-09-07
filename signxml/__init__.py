c34b7297d56f74b0b4ca6a9d685fad6b1956d226
Andrey Kislyuk <kislyuk@gmail.com>
2014-09-06 21:44:52 -0700

    WIP

--- signxml/__init__.py
+++ signxml/__init__.py
@@ -2,64 +2,98 @@ from __future__ import print_function, unicode_literals
 
 import hashlib, base64, hmac
 
+from eight import *
+
 from collections import namedtuple, Mapping, Iterable
 from lxml import etree
-from lxml.etree import Element
+from lxml.etree import Element, SubElement
 
 # TODO: use https://pypi.python.org/pypi/defusedxml/#defusedxml-lxml
 
 XMLDSIG_NS = "http://www.w3.org/2000/09/xmldsig#"
 
+def pack_bigint(i):
+    # See http://stackoverflow.com/questions/14764237
+    b = bytearray()
+    while i:
+        b.append(i & 0xFF)
+        i >>= 8
+    return b
+
 class xmldsig(object):
-    def __init__(self, data, digest_algorithm="sha1", signature_algorithm="hmac-sha1"):
+    def __init__(self, data, digest_algorithm="sha1"):
         self.digest_algo = digest_algorithm
-        self.signature_algo = signature_algorithm
+        self.signature_algo = None
         self.data = data
         self.hasher = hashlib.new(self.digest_algo)
 
-    def sign(self):
+    def _sign_hmac_sha1(self):
+        pass
+
+    def _sign_dsa_sha1(self):
+        pass
+
+    def _sign_rsa_sha1(self):
+        pass
+
+    def sign(self, algorithm="dsa-sha1", key=None, passphrase=None):
+        self.signature_algo = algorithm
+        self.key = key
         self.payload = Element("Object", Id="object")
-        from collections import OrderedDict
+        if isinstance(self.data, str):
+            self.payload.text = self.data
+        else:
+            self.payload.append(self.data)
         #self.payload.set("xmlns", XMLDSIG_NS)
-        self.payload.text = """some text
-  with spaces and CR-LF."""
-#        self.payload.append(self.data)
         sig_root = Element("Signature", xmlns=XMLDSIG_NS)
 
         c14n = etree.tostring(self.payload, method="c14n", with_comments=False, exclusive=True)
         c14n = c14n.replace("<Object", '<Object xmlns="{}"'.format(XMLDSIG_NS))
-        print("BEGIN C14N")
-        print(c14n)
-        print("END C14N")
+#        print("BEGIN C14N")
+#        print(c14n)
+#        print("END C14N")
         self.hasher.update(c14n)
         self.digest = base64.b64encode(self.hasher.digest())
-        print(self.digest)
+#        print(self.digest)
 
-        #print("PARENT:", self.payload.getparent().get("xmlns"))
-
-
-        signed_info = Element("SignedInfo", xmlns=XMLDSIG_NS)
-        sig_root.append(signed_info)
-        canonicalization_method = Element("CanonicalizationMethod", Algorithm="http://www.w3.org/2006/12/xml-c14n11")
-        signed_info.append(canonicalization_method)
-        signature_method = Element("SignatureMethod", Algorithm=XMLDSIG_NS + self.signature_algo)
-        signed_info.append(signature_method)
-        reference = Element("Reference", URI="#object")
-        signed_info.append(reference)
-        digest_method = Element("DigestMethod", Algorithm=XMLDSIG_NS + self.digest_algo)
-        reference.append(digest_method)
-        digest_value = Element("DigestValue")
+        signed_info = SubElement(sig_root, "SignedInfo", xmlns=XMLDSIG_NS)
+        canonicalization_method = SubElement(signed_info, "CanonicalizationMethod", Algorithm="http://www.w3.org/2006/12/xml-c14n11")
+        signature_method = SubElement(signed_info, "SignatureMethod", Algorithm=XMLDSIG_NS + self.signature_algo)
+        reference = SubElement(signed_info, "Reference", URI="#object")
+        digest_method = SubElement(reference, "DigestMethod", Algorithm=XMLDSIG_NS + self.digest_algo)
+        digest_value = SubElement(reference, "DigestValue")
         digest_value.text = self.digest
-        reference.append(digest_value)
-        signature_value = Element("SignatureValue")
+        signature_value = SubElement(sig_root, "SignatureValue")
+
+        signed_info_payload = etree.tostring(signed_info, method="c14n")
         if self.signature_algo == "hmac-sha1":
-            k = b"secret"
             #k = 73 65 63 72 65 74
-            hasher = hmac.new(key=k,
-                              msg=etree.tostring(signed_info, method="c14n"),
+            hasher = hmac.new(key=self.key,
+                              msg=signed_info_payload,
                               digestmod=hashlib.sha1)
             signature_value.text = base64.b64encode(hasher.digest())
             sig_root.append(signature_value)
+        elif self.signature_algo in ("dsa-sha1", "rsa-sha1"):
+            from Crypto.PublicKey import RSA, DSA
+            from Crypto.Signature import PKCS1_v1_5
+            from Crypto.Hash import SHA
+            from Crypto.Util.number import long_to_bytes
+            SA = DSA if self.signature_algo == "dsa-sha1" else RSA
+            key = SA.importKey(self.key, passphrase=passphrase)
+            digest = SHA.new(signed_info_payload)
+            signature = PKCS1_v1_5.new(key).sign(digest)
+
+            signature_value.text = base64.b64encode(signature)
+            key_info = SubElement(sig_root, "KeyInfo")
+            key_value = SubElement(key_info, "KeyValue")
+            if SA is RSA:
+                rsa_key_value = SubElement(key_value, "RSAKeyValue")
+                modulus = SubElement(rsa_key_value, "Modulus")
+                modulus.text = base64.b64encode(long_to_bytes(key.n))
+                exponent = SubElement(rsa_key_value, "Exponent")
+                exponent.text = base64.b64encode(long_to_bytes(key.e))
+        else:
+            raise NotImplementedError()
         sig_root.append(self.payload)
         return sig_root
 
--- test/test.py
+++ test/test.py
@@ -15,11 +15,32 @@ class TestSignXML(unittest.TestCase):
     def setUp(self):
         self.example_xml_file = os.path.join(os.path.dirname(__file__), "example.xml")
 
+        #from Crypto.PublicKey import RSA, DSA
+        #key = RSA.generate(4096)
+        #with open("rsa_key.pem", "wb") as fh:
+        #    fh.write(key.exportKey())
+        #key = DSA.generate(512)
+        #with open("dsa_key.pem", "wb") as fh:
+        #    fh.write(key.exportKey())
+
     def test_basic_signxml_statements(self):
         tree = etree.parse(self.example_xml_file)
 #        signature = sign(tree)
         #x = etree.Element(
-        print(etree.tostring(xmldsig(tree.getroot()).sign()))
+        k = b"secret"
+#        with open("AlicePrivRSASign_epk.txt") as fh:
+        with open("rsa_key.pem") as fh:
+            rsa_key = fh.read()
+#            for line in fh:
+#                if line.startswith("-----"):
+#                    continue
+#                rsa_key += line.strip()
+#        print("LINE:", rsa_key)
+        rsa_passphrase = "password"
+        print(etree.tostring(xmldsig("wat").sign(algorithm="rsa-sha1",
+                                                 key=rsa_key,
+                                                 passphrase=rsa_passphrase)))
+#tree.getroot()).sign()))
 #        parser.feed(open(self.example_xml_file).read())
 #        tree = parser.close()
 #        print(tree.sign())
