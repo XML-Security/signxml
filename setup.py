29b1213e8d41e28e717174455dbff0971f5af57f
Andrey Kislyuk <kislyuk@gmail.com>
2016-09-18 23:55:29 -0700

    v2.1.4

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='signxml',
-    version="2.1.3',
+    version="2.1.4",
     url='https://github.com/kislyuk/signxml',
     license='Apache Software License',
     author='Andrey Kislyuk',
