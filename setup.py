0c14f734f8723d3d10c8dd114950d176ef39c428
Michael Barrientos <mbarrien@users.noreply.github.com>
2017-04-22 10:21:26 -0700

    Allow newer versions of several requirements

--- setup.py
+++ setup.py
@@ -12,11 +12,11 @@ setup(
     description='Python XML Signature library',
     long_description=open('README.rst').read(),
     install_requires=[
-        'lxml >= 3.5.0, < 3.7',
+        'lxml >= 3.5.0, < 3.8',
         'defusedxml >= 0.4.1, < 0.6',
         'eight >= 0.3.0, < 0.5',
-        'cryptography >= 1.2.3, < 1.8',
-        'pyOpenSSL >= 0.15.1, < 17',
+        'cryptography >= 1.2.3, < 1.9',
+        'pyOpenSSL >= 0.15.1, < 18',
         'certifi >= 2015.11.20.1'
     ],
     extras_require={
