81e5a4b6089689d4699ea9b4bfe9fb9b52439100
Andrey Kislyuk <kislyuk@gmail.com>
2017-12-07 09:06:34 -0800

    Relax dependency version constraints. Drop Python 3.3 support. Fixes #100.

--- setup.py
+++ setup.py
@@ -12,17 +12,16 @@ setup(
     description='Python XML Signature library',
     long_description=open('README.rst').read(),
     install_requires=[
-        'lxml >= 3.5.0, < 4',
-        'defusedxml >= 0.4.1, < 0.6',
-        'eight >= 0.3.0, < 0.5',
-        'cryptography >= 1.8, < 2.1',
+        'lxml >= 3.5.0, < 5',
+        'defusedxml >= 0.4.1, < 1',
+        'eight >= 0.3.0, < 1',
+        'cryptography >= 1.8, < 3',
         'asn1crypto >= 0.21.0',
         'pyOpenSSL >= 0.15.1, < 18',
         'certifi >= 2015.11.20.1'
     ],
     extras_require={
-        ':python_version == "2.7"': ['enum34 >= 1.0.4'],
-        ':python_version == "3.3"': ['enum34 >= 1.0.4']
+        ':python_version == "2.7"': ['enum34 >= 1.0.4']
     },
     packages=find_packages(exclude=['test']),
     platforms=['MacOS X', 'Posix'],
@@ -37,7 +36,6 @@ setup(
         'Operating System :: POSIX',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules'
