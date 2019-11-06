2653ad8050ecc4540b0792323b8e4e756d08df05
David Arnold <dar@xoe.solutions>
2019-11-06 16:18:34 -0500

    WIP WIP

--- signxml/__init__.py
+++ signxml/__init__.py
@@ -282,6 +282,12 @@ class XMLSigner(XMLSignatureProcessor):
         self.c14n_alg = c14n_algorithm
         self.namespaces = dict(ds=namespaces.ds)
         self._parser = None
+        """
+        List of built out elements to incorporate
+        such as XADES scheme or any other
+        :type to_append: py:class: list
+        """
+        self.to_append = []
 
     def sign(
         self,
