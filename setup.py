f3b7f03098f30f1ca405063214d1cce796f4a305
Andrey Kislyuk <kislyuk@gmail.com>
2024-01-27 10:59:46 -0800

    Bump dependency versions and require cryptography 42

--- setup.py
+++ setup.py
@@ -11,14 +11,13 @@ setup(
     author_email="kislyuk@gmail.com",
     description="Python XML Signature and XAdES library",
     long_description=open("README.rst").read(),
-    python_requires=">=3.7",
+    python_requires=">=3.8",
     install_requires=[
         # Dependencies are restricted by major version range according to semver.
-        # By default, version minimums are set to be compatible with the oldest supported Ubuntu LTS (currently 18.04).
-        "lxml >= 4.2.1, < 5",
-        "cryptography >= 3.4.8",  # Set to the version in Ubuntu 22.04 due to features we need from cryptography 3.1
-        "pyOpenSSL >= 17.5.0",
-        "certifi >= 2018.1.18",
+        # By default, version minimums are set to be compatible with the oldest supported Ubuntu LTS.
+        "lxml >= 4.5.0, < 6",
+        "cryptography >= 42.0.1",  # We require cryptography 42
+        "certifi >= 2019.11.28",
         # "tsp-client >= 0.1.3",
     ],
     extras_require={
@@ -42,11 +41,11 @@ setup(
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
         "Programming Language :: Python",
-        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
